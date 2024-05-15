ac85d2f4e50c9542e3a85e15b192e0de3ddc49ce
Ben <bgoober2@protonmail.com>
2024-05-15 14:57:32 -0400

    1st week, class # 1 updates

--- ts/cluster1/spl_init.ts
+++ ts/cluster1/spl_init.ts
@@ -1,9 +1,10 @@
 import { Keypair, Connection, Commitment } from "@solana/web3.js";
 import { createMint } from '@solana/spl-token';
-import wallet from "../wba-wallet.json"
+import wallet from "../wba-wallet.json" // import wallet from file
+import { connect } from "http2";
 
-// Import our keypair from the wallet file
-const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
+// Import our keypair from the wallet
+const keypair = Keypair.fromSecretKey(new Uint8Array(wallet)); 
 
 //Create a Solana devnet connection
 const commitment: Commitment = "confirmed";
@@ -12,7 +13,12 @@ const connection = new Connection("https://api.devnet.solana.com", commitment);
 (async () => {
     try {
         // Start here
-        // const mint = ???
+        // create mint authority with our wallet keypair. Our wallet is also the authority of the mint
+        const mint = await createMint(connection, keypair, keypair.publicKey, null, 6);
+        // log to base 58
+        console.log(`Your mint address is: ${mint.toBase58()}`);
+
+
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
--- ts/cluster1/spl_mint.ts
+++ ts/cluster1/spl_mint.ts
@@ -5,24 +5,26 @@ import wallet from "../wba-wallet.json"
 // Import our keypair from the wallet file
 const keypair = Keypair.fromSecretKey(new Uint8Array(wallet));
 
-//Create a Solana devnet connection
+//Create a Solana devnet connection and set the commitment to confirmed (so the "await" function in line 20 will wait for the transaction to be confirmed)
 const commitment: Commitment = "confirmed";
-const connection = new Connection("https://api.devnet.solana.com", commitment);
+const connection = new Connection("https://api.devnet.solana.com", commitment); // connection to devnet
 
-const token_decimals = 1_000_000n;
+const token_decimals = 1_000_000n; // 6 decimals (n means bigint in typescript)
 
-// Mint address
-const mint = new PublicKey("<mint address>");
+// Mint address (derived from the spl_init.ts file and its createMint function output)
+const mint = new PublicKey("Dr4QQ3bASMt9RvAHaNd6Bx3kGfwWrNJZThNjDgENSwbR");
 
 (async () => {
     try {
-        // Create an ATA
-        // const ata = ???
-        // console.log(`Your ata is: ${ata.address.toBase58()}`);
+        // Create an ATA, with your keypair as the owner of the token account, and the mint account as the mint of the token account
+        // the "mint" is simply the mint address of the token you want to mint. Each token has its own mint address.
+        const ata = await getOrCreateAssociatedTokenAccount(connection, keypair, mint, keypair.publicKey);
+        console.log(`Your ata is: ${ata.address.toBase58()}`);
+    
 
-        // Mint to ATA
-        // const mintTx = ???
-        // console.log(`Your mint txid: ${mintTx}`);
+        // Mint to the ATA we created for our keypair, to hold the tokens we mint, which are only associated with the mint address
+        const mintTx = await mintTo(connection, keypair, mint, ata.address, keypair, 100n * token_decimals);
+        console.log(`Your mint txid: ${mintTx}`);
     } catch(error) {
         console.log(`Oops, something went wrong: ${error}`)
     }
